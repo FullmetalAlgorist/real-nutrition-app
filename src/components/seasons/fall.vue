8950fb5f440c8eb045b64373a0f5f99af0505011
akcrew <mariah.vicary@gmail.com>
2019-05-21 21:12:12 -0700

    macbook

--- src/components/seasons/fall.vue
+++ src/components/seasons/fall.vue
@@ -55,8 +55,9 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .fall{
+
   background: #333333;  /* fallback for old browsers */
   background: -webkit-linear-gradient(to bottom
   right, #dd1818, #333333);  /* Chrome 10-25, Safari 5.1-6 */
